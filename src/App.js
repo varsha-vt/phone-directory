9e87ecc1f3664a8d8eeefcf7f0b4da0216def1fd
thomasv <thomasv@vmware.com>
2021-04-24 17:16:25 +0530

    basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
 function App() {
-  return (
-    <div>
-      Phone Directory
-    </div>
-  );
+
+    return (
+        <div>
+            <div className="header">
+                Phone Directory
+            </div>
+            <button> Add</button>
+            <div>
+                <span>Name </span><br/>
+                <span>Phone </span>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
