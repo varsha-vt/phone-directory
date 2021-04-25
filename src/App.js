4ce9624321f8bb2ff42c6f17e0816fa02b50ead6
thomasv <thomasv@vmware.com>
2021-04-25 13:46:10 +0530

    adding style and dynamic rendring of subscribers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-function App() {
+import React, {Component} from 'react';
+import Header from  "./Header";
+import './App.css'
 
+function App() {
+    let subscribers = [
+        {
+            id: 1,
+            name: "Varsha Thomas",
+            phone: "8888888"
+        },
+        {
+            id: 2,
+            name: "Rohan Sartho",
+            phone: "9999999"
+        }
+    ]
     return (
         <div>
-            <div className="header">
-                Phone Directory
-            </div>
-            <button> Add</button>
-            <div>
-                <span>Name </span><br/>
-                <span>Phone </span>
+            <Header/>
+            <div className="component-body-container">
+                <button className="custom-btn add-btn"> Add</button>
+                <div className="grid-container heading-container">
+                    <span className="grid-item name-heading">Name </span>
+                    <span className="grid-item phone-heading">Phone </span>
+                </div>
+                {
+                    subscribers.map(sub=> {
+                        return  <div key={sub.id} className="grid-container">
+                           <span className="grid-item">{sub.name}</span> 
+                           <span className="grid-item">{sub.phone}</span> 
+                           </div>
+                    })
+                }
+
             </div>
         </div>
 
